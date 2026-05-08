500a7e6b79ac65d5e654271e82c9b871b7699b13
networkisvn-cell <networkisvn@gmail.com>
2026-05-08 22:18:11 +0600

    Update ContactTab.tsx

--- src/components/ContactTab.tsx
+++ src/components/ContactTab.tsx
@@ -11,20 +11,20 @@ const contacts: ContactLink[] = [
   {
     icon: <MessageCircle size={14} />,
     label: 'Discord',
-    value: 'arcticayl',
-    href: 'https://discord.com/users/arcticayl',
+    value: 'vemime',
+    href: 'https://discord.com/users/noxe',
   },
   {
     icon: <Github size={14} />,
     label: 'GitHub',
-    value: '@irenic.alyaa',
-    href: 'https://github.com/irenic.alyaa',
+    value: '@venome2',
+    href: 'https://github.com/Venome2',
   },
   {
     icon: <Mail size={14} />,
     label: 'Email',
-    value: 'businesswithmikasa@gmail.com',
-    href: 'mailto:businesswithmikasa@gmail.com',
+    value: 'giveawayskeleton3@gmail.com',
+    href: 'mailto:giveawayskeleton3@gmail.com',
   },
 ];
 
